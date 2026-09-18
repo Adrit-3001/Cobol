69765cfb5dbeaf538ad978492511325f01405d9c
adrit <adrit.panday@mail.utoronto.ca>
2026-09-18 12:08:55 -0400

    balance example

--- hello.cob
+++ hello.cob
@@ -4,17 +4,16 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
 
-       01 A      PIC 99 VALUE 10.
-       01 B      PIC 9 VALUE 5.
-       01 C      PIC 9(3) VALUE 000.
+       01 balance      PIC 9(5) VALUE 0.
+    *>    01 B      PIC 9 VALUE 5.
+    *>    01 C      PIC 9(3) VALUE 000.
 
        PROCEDURE DIVISION.
-           COMPUTE C = A + B.
-           DISPLAY "ADdition is: " C.
-           COMPUTE C = A - B.
-           DISPLAY "Subtraction is: " C.
-           COMPUTE C = A * B.
-           DISPLAY "Multiplication is: " C.
-           COMPUTE C = A / B.
-           DISPLAY "Division is: " C.
-           STOP RUN.
+            DISPLAY "ENTER YOUR BALANCE: ".
+            ACCEPT balance.
+            IF balance > 10000
+                DISPLAY "SUFFICIENT BALANCE."
+            ELSE
+                DISPLAY "LOW BALANCE."
+            END-IF.
+            STOP RUN.
