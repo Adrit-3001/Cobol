b33453954c9d23f11befc9182a2154f735d476a2
adrit <adrit.panday@mail.utoronto.ca>
2026-09-18 12:55:32 -0400

    multiplication of user input

--- hello.cob
+++ hello.cob
@@ -4,16 +4,21 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
 
-       01 balance      PIC 9(5) VALUE 0.
-    *>    01 B      PIC 9 VALUE 5.
-    *>    01 C      PIC 9(3) VALUE 000.
+       01 num      PIC 9(5) VALUE 0.
+       01 i      PIC 99 VALUE 1.
+       01 RESULT      PIC 9(3) VALUE 000.
 
        PROCEDURE DIVISION.
-            DISPLAY "ENTER YOUR BALANCE: ".
-            ACCEPT balance.
-            IF balance > 10000
-                DISPLAY "SUFFICIENT BALANCE."
+            DISPLAY "ENTER num: ".
+            ACCEPT num.
+            IF num >= 10
+                DISPLAY "not single digit."
+                STOP RUN
             ELSE
-                DISPLAY "LOW BALANCE."
+                PERFORM UNTIL i > 10
+                    COMPUTE RESULT = num * i
+                    DISPLAY RESULT
+                    ADD 1 TO i
+                END-PERFORM
             END-IF.
             STOP RUN.
